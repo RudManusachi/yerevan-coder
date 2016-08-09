90d143bad90a32523ab09874782f93cecc5e4251
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-08-09 04:05:32 -0700

    Add nodemon to package.json, add nav bar

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -2,14 +2,33 @@
 
 const React = require('react');
 
+const NavBar = React.createClass({
+  render() {
+    let style = {listStyleType:"none"};
+    let li_style = {display:'inline'};
+    return (
+      <nav>
+	<ul style={style}>
+	  <li style={li_style}> About </li>
+	  <li style={li_style}> Writing </li>
+	</ul>
+      </nav>
+    );
+  }
+});
+
 const HomePage = React.createClass({
   render() {
+    let style = {margin:0,
+		 padding: 0,
+		 backgroundColor: 'aliceblue'};
     return (
       <html>
 	<head>
 	  <title> yerevancoder.com </title>
 	</head>
-	<body>
+	<body style={style}>
+	  <NavBar/>
 	  <div>
 	    <p> This is a sample Home Page </p>
 	  </div>
