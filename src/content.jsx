629d32aedb6ef3cc314a65ee370ad26b04bd55cc
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-08-13 10:41:09 -0700

    General outline of site created

---
+++ src/content.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+module.exports = class _ extends React.Component {
+  render() {
+    return (
+      <div>
+	Main Place to do the action
+      </div>
+    );
+  }
+};
--- src/homepage.jsx
+++ src/homepage.jsx
@@ -2,38 +2,44 @@
 
 const React = require('react'),
       ReactDOM = require('react-dom'),
-      Bar = require('./topbar.jsx');
-
-// class Counter extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {count: props. initial_count};
-//     this.tick = this.tick.bind(this);
-//   }
-
-//   tick() {
-//     this.setState({count:this.state.count + 1});
-//   }
-
-//   render() {
-//     return (
-//       <div onClick={this.tick}>
-// 	Clicks: {this.state.count}
-// 	<Bar/>
-//       </div>
-//     );
-//   }
-
-// };
-
-// Counter.propTypes = { initial_count : React.PropTypes.number};
-// Counter.defaultProps = { initial_count : 0};
+      SideBar = require('./sidebar.jsx'),
+      Content = require('./content.jsx');
 
 class HomePage extends React.Component {
   render() {
+    let site_style =
+	{
+	  display:'flex',
+	  paddingLeft:'2em',
+	  paddingTop:'2em',
+	  paddingRight:'2em'
+	};
+
+    let ul_style =
+	{
+	  marginLeft:'3em',
+	  listStyleType:'none'
+	};
+
+    let banner_style =
+	{
+	  backgroundColor:'#f1f1f1',
+	  padding:'1em 1em 1em 1em'
+	};
+
     return (
-      <div>
-	<Bar/>
+      <div style={site_style}>
+	<SideBar/>
+	<ul style={ul_style}>
+	  <li>
+	    <h3 style={banner_style}>
+	      From San Francisco, USA to Yerevan, Armenia
+	    </h3>
+	  </li>
+	  <li>
+	    <Content/>
+	  </li>
+	</ul>
       </div>
     );
   }
