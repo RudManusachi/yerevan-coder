d3f5f9808b1707361e41d8dd4add485621a40fc5
Vianka Lemus <vianka.lemus@gmail.com>
2016-09-26 02:54:38 -0700

    png trans background & adjustments

--- lib/header.jsx
+++ lib/header.jsx
@@ -7,47 +7,48 @@ class Header extends React.Component {
   render() {
     let style = {
       flexDirection: 'row',
-      justifyContent: 'center',
+    
       whiteSpace: 'noWrap',
-      height: '6rem',
-      width: '100%',
+     
+     
       backgroundColor: 'rgba(225,135,40,0.2)',
       color: '#484848',
-      padding: '1rem'
+      padding: '1em'
     };
-    let hstyle = {
-      fontStyle: 'italic',
-      textAlign: 'center',
-      paddingLeft: '2rem',
-      paddingTop: '1rem',
-      paddingBottom: '.5rem',
-      paddingRight: '2rem',
-      fontSize: '3rem',
-      marginBottom: '1rem', 
+
+   
+
+    let head = {
+      paddingLeft: '6em',
+      
+      maxWidth: '30em',
       display: 'inline'
       
     };
-    let logo = {
-      width: '100px',
-      height: '100px',
-      borderRadius:'50%',
-      overflow: 'hidden'
-    };
+
     let sidelink = {
       display: 'inline',
-      fontFamily: 'helvetica',
       justifyContent: 'flexEnd',
       alignItems: 'flexEnd',
-      padding: '.5rem'
+      padding: '2em',
+      float: 'right',
+      listStyle: 'none'
+    };
+
+     let item = {
+      display: 'inline',
+      padding: '1em'
     };
 
     return (
       <div style={style}>
-	<img style={logo} src='public/yclogo.png'/>
-	<h4 style={hstyle}>YEREVAN CODER</h4>
-	<h6 style={sidelink}>LOGIN</h6>
-	<h6 style={sidelink}>SIGNUP</h6>
-	<h6 style={sidelink}>FORGOT</h6>
+
+	       <img style={head} src='public/ycHEAD.png'/>
+         <ul style={sidelink}>
+	         <li style={item}>Login </li>
+	         <li style={item}>SignUp </li>
+	         <li style={item}>Forgot </li>
+         </ul>
       </div>
     );
   }
